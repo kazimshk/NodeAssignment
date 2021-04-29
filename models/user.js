64bcbc0f991d5c66b7c9991472c0c694f86e0eaa
kazimshk <kazimshk@gmail.com>
2021-04-29 19:09:15 +0500

    Uptill JWT Done

--- models/user.js
+++ models/user.js
@@ -1,43 +1,52 @@
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const postsSchema = require('./posts');
-
-// const postsSchema = new mongoose.Schema({
-//     title : {
-//         type: String,
-//         required: true
-//     }
-//     // ,
-//     // description : {
-//     //     type: String,
-//     //     required: true
-//     // },
-//     // postDate : {
-//     //     type: Date,
-//     //     required: true,
-//     //     default: Date.now
-//     // }
-// })
-// const posts = mongoose.model('post', postsSchema);
+const postsSchema = require("./posts");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-    name : {
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  joinigDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "posts",
     },
-    email : {
-        type: String,
-        required: true
+  ],
+  followers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
     },
-    joinigDate : {
-        type: Date,
-        required: true,
-        default: Date.now
+  ],
+  following: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
     },
-    posts :[{
-        type: Schema.Types.ObjectId,
-        ref: 'posts'
-    }]
-})
+  ],
+});
 
-module.exports = mongoose.model('user', userSchema);
+userSchema.methods.generateAuthToken = function () {
+  console.log("env :" + process.env.ACCESS_TOKEN_SCERET);
+  const token = jwt.sign(
+    { _id: this._id },
+    "c23409cjo23kj209hfd0jdsk203u0rj230rhi2ckj312" /*process.env.ACCESS_TOKEN_SCERET*/
+  );
+  console.log("env :" + process.env.ACCESS_TOKEN_SCERET);
+  console.log("methods token:  " + token);
+  return token;
+};
+
+module.exports = mongoose.model("user", userSchema);
--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-//const subscriberRouter = require("./routes/subscribers");
+const mongoose = require("mongoose");
 const userRouter = require("./routes/users");
+const postRouter = require("./routes/posts");
 
 //Database Connection
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true,useUnifiedTopology: true });
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 
 //To check whether DB is connected or not
-db.on('error',(error)=> console.error(error));
-db.once('open', ()=>console.log("Connected to DataBase"));      
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to DataBase"));
 
 app.use(express.json());
 
 //Routes
-//app.use('/subscribers', subscriberRouter);
-app.use('/users', userRouter);
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
 
-app.listen(3000, ()=>{
-    console.log("server is running at 3000 port");
-})
+app.listen(3000, () => {
+  console.log("server is running at 3000 port");
+});
